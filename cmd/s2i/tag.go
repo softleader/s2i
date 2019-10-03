fbef45191b6db28fbb2216e3367979ba9c0b85f5
matt <methodho@gmail.com>
2019-10-03 17:00:55 +0800

    fix typo

--- cmd/s2i/tag.go
+++ cmd/s2i/tag.go
@@ -4,15 +4,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const pluginTagDesc = `
+const pluginTagDesc = `更方便的管理 GitHub 上的 tags
 `
 
 func neTagCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "tag",
-		Aliases: []string{"pre"},
-		Short:   "tag",
-		Long:    pluginTagDesc,
+		Use:   "tag",
+		Short: "manage tags on GitHub",
+		Long:  pluginTagDesc,
 	}
 	cmd.AddCommand(
 		newTagListCmd(),
